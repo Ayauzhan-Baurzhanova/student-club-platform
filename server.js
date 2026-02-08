c0d4f53630eb7ff91b7e22a5ca22af58dd338c19
Ayauzhan-Baurzhanova <241614@astanait.edu.kz>
2026-02-08 15:45:42 +0600

    Update server.js
    fix render issue

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/requests", requestRoutes);
 
 // --- Catch-All Route ---
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
